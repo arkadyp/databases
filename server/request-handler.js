da45f221ff7312507e7d5f2ea5454709605f210d
Hack Reactor Students <students@hackreactor.com>
2014-01-27 15:03:51 -0800

    Creates testResponse messages and delivers them to client

--- server/request-handler.js
+++ server/request-handler.js
@@ -5,6 +5,16 @@
  * this file and include it in basic-server.js so that it actually works.
  * *Hint* Check out the node module documentation at http://nodejs.org/api/modules.html. */
 
+var testResponse = {"results":
+  [
+    {"username":"pinky_pie","text":"Oh, gosh. Node is sooOOoo hard!","roomname":"nodejs","createdAt":"2014-01-27T20:27:40.528Z","updatedAt":"2014-01-27T20:27:40.528Z","objectId":"ltH2Va7xKe"},
+    {"username":"bv","text":"Nothing life the smell of node in the morning ","roomname":"nodejs","createdAt":"2014-01-27T20:18:58.670Z","updatedAt":"2014-01-27T20:18:58.670Z","objectId":"e1T2TkeKCg"},
+    {"username":"bv","text":"...","roomname":"main","createdAt":"2014-01-27T20:01:08.511Z","updatedAt":"2014-01-27T20:01:08.511Z","objectId":"6bTN1V0Dnp"},
+    {"username":"aut","text":"hiya","roomname":"main","createdAt":"2014-01-27T19:57:55.977Z","updatedAt":"2014-01-27T19:57:55.977Z","objectId":"QA9OcXO3cq"},
+    {"username":"David","text":"tesstststs","roomname":"heeeeeeeeeeelp","createdAt":"2014-01-27T19:56:17.881Z","updatedAt":"2014-01-27T19:56:17.881Z","objectId":"vrA125C5BU"},
+  ]
+};
+
 var handleRequest = function(request, response) {
   /* the 'request' argument comes from nodes http module. It includes info about the
   request - such as what URL the browser is requesting. */
@@ -13,6 +23,9 @@ var handleRequest = function(request, response) {
    * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
 
   console.log("Serving request type " + request.method + " for url " + request.url);
+  var url = require('url').parse(request.url);
+  console.log(url);
+
 
   var statusCode = 200;
 
@@ -29,7 +42,8 @@ var handleRequest = function(request, response) {
    * anything back to the client until you do. The string you pass to
    * response.end() will be the body of the response - i.e. what shows
    * up in the browser.*/
-  response.end("Hello, World!");
+  response.write(JSON.stringify(testResponse));
+  response.end();
 };
 
 /* These headers will allow Cross-Origin Resource Sharing (CORS).
