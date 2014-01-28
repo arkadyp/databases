e9044522d93c5a9295b0f292dae468e545ea2a3c
Hack Reactor Students <students@hackreactor.com>
2014-01-27 20:12:48 -0800

    handles URLs and responds with appropriate status

--- server/request-handler.js
+++ server/request-handler.js
@@ -7,11 +7,11 @@
 
 var testResponse = {"results":
   [
-    {"username":"pinky_pie","text":"Oh, gosh. Node is sooOOoo hard!","roomname":"nodejs","createdAt":"2014-01-27T20:27:40.528Z","updatedAt":"2014-01-27T20:27:40.528Z","objectId":"ltH2Va7xKe"},
-    {"username":"bv","text":"Nothing life the smell of node in the morning ","roomname":"nodejs","createdAt":"2014-01-27T20:18:58.670Z","updatedAt":"2014-01-27T20:18:58.670Z","objectId":"e1T2TkeKCg"},
-    {"username":"bv","text":"...","roomname":"main","createdAt":"2014-01-27T20:01:08.511Z","updatedAt":"2014-01-27T20:01:08.511Z","objectId":"6bTN1V0Dnp"},
-    {"username":"aut","text":"hiya","roomname":"main","createdAt":"2014-01-27T19:57:55.977Z","updatedAt":"2014-01-27T19:57:55.977Z","objectId":"QA9OcXO3cq"},
-    {"username":"David","text":"tesstststs","roomname":"heeeeeeeeeeelp","createdAt":"2014-01-27T19:56:17.881Z","updatedAt":"2014-01-27T19:56:17.881Z","objectId":"vrA125C5BU"},
+    // {"username":"pinky_pie","text":"Oh, gosh. Node is sooOOoo hard!","roomname":"nodejs","createdAt":"2014-01-27T20:27:40.528Z","updatedAt":"2014-01-27T20:27:40.528Z","objectId":"ltH2Va7xKe"},
+    // {"username":"bv","text":"Nothing life the smell of node in the morning ","roomname":"nodejs","createdAt":"2014-01-27T20:18:58.670Z","updatedAt":"2014-01-27T20:18:58.670Z","objectId":"e1T2TkeKCg"},
+    // {"username":"bv","text":"...","roomname":"main","createdAt":"2014-01-27T20:01:08.511Z","updatedAt":"2014-01-27T20:01:08.511Z","objectId":"6bTN1V0Dnp"},
+    // {"username":"aut","text":"hiya","roomname":"main","createdAt":"2014-01-27T19:57:55.977Z","updatedAt":"2014-01-27T19:57:55.977Z","objectId":"QA9OcXO3cq"},
+    // {"username":"David","text":"tesstststs","roomname":"heeeeeeeeeeelp","createdAt":"2014-01-27T19:56:17.881Z","updatedAt":"2014-01-27T19:56:17.881Z","objectId":"vrA125C5BU"},
   ]
 };
 
@@ -22,11 +22,26 @@ var handleRequest = function(request, response) {
   /* Documentation for both request and response can be found at
    * http://nodemanual.org/0.8.14/nodejs_ref_guide/http.html */
 
-  console.log("Serving request type " + request.method + " for url " + request.url);
-  var url = require('url').parse(request.url);
-  console.log(url);
+  // console.log("Serving request type " + request.method + " for url " + request.url);
+  // console.log(url);
 
-  if (request.method === "POST"){
+  var endResponse = function(statusCode) {
+    var headers = defaultCorsHeaders;
+    headers['Content-Type'] = "text/plain";
+    response.writeHead(statusCode, headers);
+    response.write(JSON.stringify(testResponse));
+    response.end();
+  };
+
+  var url = require('url').parse(request.url);
+  if(url.path.slice(0, 8) !== '/classes'){
+    endResponse(404);
+  }
+
+  console.log(request.method);
+  if(request.method === 'OPTIONS') {
+    endResponse(200);
+  } else if (request.method === "POST" && url.path.slice(url.path.length - 5, url.path.length) === '/send'){
     var msg = "";
     request.on('data', function (data) {
       msg += data;
@@ -35,34 +50,16 @@ var handleRequest = function(request, response) {
       msg = JSON.parse(msg);
       msg.createdAt = new Date();
       msg.updatedAt = new Date();
-      testResponse.results.push(msg);
+      testResponse.results.unshift(msg);
+      endResponse(201);
     })
+  } else if(request.method === 'GET'){
+    endResponse(200);
+  } else {
+    endResponse(404);
   }
-
-  var statusCode = 200;
-
-  /* Without this line, this server wouldn't work. See the note
-   * below about CORS. */
-  var headers = defaultCorsHeaders;
-
-  headers['Content-Type'] = "text/plain";
-
-  /* .writeHead() tells our server what HTTP status code to send back */
-  response.writeHead(statusCode, headers);
-
-  /* Make sure to always call response.end() - Node will not send
-   * anything back to the client until you do. The string you pass to
-   * response.end() will be the body of the response - i.e. what shows
-   * up in the browser.*/
-  response.write(JSON.stringify(testResponse));
-  response.end();
 };
 
-/* These headers will allow Cross-Origin Resource Sharing (CORS).
- * This CRUCIAL code allows this server to talk to websites that
- * are on different domains. (Your chat client is running from a url
- * like file://your/chat/client/index.html, which is considered a
- * different domain.) */
 var defaultCorsHeaders = {
   "access-control-allow-origin": "*",
   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
