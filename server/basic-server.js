a722b434e6cc3efdb047b887f8252eb953aa80b8
Hack Reactor Students <arkady.pevzner@gmail.com>
2014-01-31 12:17:49 -0800

    Merged in chatterbox-server

--- server/basic-server.js
+++ server/basic-server.js
@@ -14,11 +14,11 @@ var routes = {
   "/bower_components/jquery/jquery.min.js"         : {'path':'client/bower_components/jquery/jquery.min.js',          'type':'text/javascript'},
   "/bower_components/underscore/underscore-min.js" : {'path':'client/bower_components/underscore/underscore-min.js',  'type':'text/javascript'},
   "/bower_components/jquery/jquery.min.map"        : {'path':'client/bower_components/jquery/jquery.min.map',         'type':'text/javascript'},
-  "/bower_components/underscore/underscore-min.map": {'path':'client/bower_components/underscore/underscore-min.map', 'type':'text/javascript'},
+  "/bower_components/underscore/underscore-min.map": {'path':'client/bower_components/underscore/underscore-min.map', 'type':'text/javascript'}
 };
 
 var server = http.createServer(function(req,res){
-  
+
   if (!req.url.match("classes")){
     var router = function(req,res){
       if (routes[req.url]){
@@ -29,7 +29,7 @@ var server = http.createServer(function(req,res){
         res.writeHead(404, {'Content-Type': 'text/html'});
         res.end('<html><head><link rel="stylesheet" href="/styles/styles.css"></head><body style="text-align: center"><h1>IDIOT</h1></body></html>');
       }
-    }
+    };
     router(req,res);
   }
   handler.handleRequest(req, res);
