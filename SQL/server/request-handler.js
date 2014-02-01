13418e1df88c10700c92859266c57b5cfebf30b3
Hack Reactor Students <arkady.pevzner@gmail.com>
2014-01-31 20:53:26 -0800

    accepts new users and posts messages from them

--- SQL/server/request-handler.js
+++ SQL/server/request-handler.js
@@ -4,84 +4,72 @@ var helpers = require('./http-helpers');
 var database = require('./database');
 var url = require('url');
 
-var buildQueryString = function(req){
+var buildGetQueryString = function(req){
   var options = require('querystring').parse(req.url);
   var responseStr = "select b.username, a.text, c.roomname from messages a inner join users b on a.id_users = b.id inner join rooms c on c.id = a.id_rooms;";
 
+  //filter roomname
   if(options.where) {
     options.where = JSON.parse(options.where);
-    console.log(options.where);
     if(options.where.roomname) {
       responseStr = responseStr.substring(0, responseStr.length - 1); //slice off semi colon
       responseStr += " and c.roomname = \"" + options.where.roomname + "\";";
     }
   }
 
+  //order messages chronologically
   if (options.order === "-createdAt"){
     responseStr = responseStr.substring(0, responseStr.length - 1); //slice off semi colon
     responseStr += " order by a.created_at desc;";
   }
 
+  //limit # of msgs
   if (options.limit){
     responseStr = responseStr.substring(0, responseStr.length - 1); //slice off semi colon
     responseStr += " LIMIT 0, " + options.limit + ";";
   }
 
-console.log(responseStr);
-return responseStr;
-
-// { '/classes/': '',
-//   order: '-createdAt',
-//   limit: '15',
-//   where: '{"roomname":"room2"}' }
-// */
-
-  // if (options.where.roomname){
-    // SELECT * FROM #temp LIMIT 0, ????
-  // }
-
-/*
-select b.username, a.text, c.roomname 
-from messages a 
-left join users b 
-on a.id_users = b.id 
-left join rooms c 
-on c.id = a.id_rooms;";
-*/
-
-
-/*
-select a.username, b.text
-into #temp
-from users a
-left join messages b
-  on a.id = b.id_users
-  1.) and roomname = 
-  2.) order by _______
-
-select *
-from #temp
-limit 0, ?????
-*/
-
-  //check for room
-
-  //check limit
-
-  //check order
-/*
-{ '/classes/': '',
-  order: '-createdAt',
-  limit: '15',
-  where: '{"roomname":"room2"}' }
-*/
-  // var responseStr = ";";
-  // return responseStr;
+  return responseStr;
 };
+var buildPostQueryString = function(message, cb){
+  var userId, roomId;
+
+  var lookupUserId = function(cb){
+    database.queryDB("select distinct id from users where username = \"" + message.username + "\";", function(err, rows, fields){
+      if(rows[0]) {
+        userId = rows[0].id;
+      }
+      cb();
+    });
+  };
+
+  var createNewUser = function(username, cb){
+    database.queryDB("insert into users (username, created_at) values ('" + username + "', now());", function(){
+        lookupUserId(cb);
+    });
+  };
+
+  var continueQuery = function(){
+    database.queryDB("select distinct id from rooms where roomname = \"" + message.roomname + "\";", function(err, rows, fields){
+      roomId = rows[0].id;
+      console.log("user: " + userId + " and room: " + roomId);
+      cb('insert into messages (id_users, id_rooms, text, created_at) values (' + userId + ',' + roomId + ',\"' + message.text + '\", now());');
+    });
+  };
+
+  lookupUserId(function(){
+    if(!userId) {
+      createNewUser(message.username, continueQuery);
+    } else {
+      continueQuery();
+    }
+  });
+};
+
 
 var getMessages = function(req, response) {
   console.log('GET attempt');
-  var querystring = buildQueryString(req);
+  var querystring = buildGetQueryString(req);
   database.queryDB(querystring, function(err, data){
     if(err) { throw err; }
     helpers.sendResponse(response, {'results': data}, 200);
@@ -90,6 +78,15 @@ var getMessages = function(req, response) {
 
 var createMessage = function(req, response) {
   console.log('post attempt');
+  helpers.collectData(req, function(message){
+    buildPostQueryString(message, function(postQuery){
+      database.queryDB(postQuery, function(err, data){
+        if (err) throw err;
+        helpers.sendResponse(response, data, 200);
+      });
+    });
+
+  });
   helpers.sendResponse(response, null);
 };
 
