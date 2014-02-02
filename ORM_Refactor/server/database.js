672aad88c99edfc7a6b91b67892c0f645b858d1b
Hack Reactor Students <arkady.pevzner@gmail.com>
2014-02-01 17:15:59 -0800

    Starts to implement ORM database

--- ORM_Refactor/server/database.js
+++ ORM_Refactor/server/database.js
@@ -1,28 +1,101 @@
 /////////////////////////////////////////////////////
 //SET UP DATABASE CONNECTION
 /////////////////////////////////////////////////////
-var mysql = require('mysql');
-var dbConnection;
 
-exports.createDBConnection = function(){
-  dbConnection = mysql.createConnection({
-    user: "root",
-    password: "",
-    database: "blackopps2"
+var Sequelize = require("sequelize");
+var sequelize = new Sequelize("blackopps3", "root", null);
+
+
+/////////////////////////////////////////////////////
+//SET UP TABLE STRUCTURE
+/////////////////////////////////////////////////////
+var User = sequelize.define('User', {
+  user_name: {type: Sequelize.STRING, unique: true}
+});
+
+var Message = sequelize.define('Message', {
+  text: Sequelize.STRING
+});
+
+var Room = sequelize.define('Room', {
+  room_name: Sequelize.STRING
+});
+
+/////////////////////////////////////////////////////
+//DEFINE RELATIONSHIPS
+/////////////////////////////////////////////////////
+Message.belongsTo(User, { foreignKey: 'user_id'});
+Message.belongsTo(Room, { foreignKey: 'room_id'});
+
+/////////////////////////////////////////////////////
+//INITIALIZE ROOMS
+/////////////////////////////////////////////////////
+// User.sync();
+// Room.sync();
+// Message.sync();
+
+// User.build({user_name: 'user1'}).save();
+// Room.build({room_name: 'room1'}).save();
+
+
+var user = 'user2';
+var roomname = 'room1';
+var text = 'abc123';
+
+var buildMessage  = function(user_name, room_name, text) {
+  //get user ID
+  User.find({where: {user_name: user_name} }).success(function(user) {
+    console.log(user);
+    console.log(user.id);
+    var user_id = user.id;
+    //get room ID
+    Room.find({where: {room_name: room_name} }).success(function(room) {
+      var room_id = room.id;
+      //compose message
+      Message.build({text: text, user_id: user_id, room_id: room_id}).save().success(function(){
+        console.log('message saved!');
+      });
+    });
   });
-  dbConnection.connect();
 };
 
-exports.queryDB = function(queryString, cb) {
-  dbConnection.query(queryString, function(err, rows, fields) {
-    if (err) throw err;
-    cb(err, rows, fields);
-  });
-};
+buildMessage(user, roomname, text);
 
-exports.closeDBConnection = function(){
-  if(dbConnection) {
-    dbConnection.end();
-  }
-};
+
+
+
+
+
+
+// drop table Messages; drop table Rooms; drop table Users;
+
+/* .sync() makes Sequelize create the database table for us if it doesn't
+ *  exist already: */
+// User.sync().success(function() {
+  // User.find({where: {user_name: 'Jean Paul'} }).success(function(user) {
+  //   var user_id = user.id;
+  //   Message.build({user_id: user_id, text: 'abc123', })
+  // });
+
+
+  // //instantiate an object and save it:
+  // var newUser = User.build({user_name: "Jean Paul"});
+  // newUser.save().success(function() {
+
+  //   var newMessage = Message.build({text: 'Hello world'});
+
+  //   /* This callback function is called once saving succeeds. */
+
+  //   // Retrieve objects from the database:
+  //   User.findAll({ where: {user_name: "Jean Paul"} }).success(function(usrs) {
+  //     // This function is called back with an array of matches.
+  //     for (var i = 0; i < usrs.length; i++) {
+  //       console.log(usrs[i].user_name + " exists");
+  //     }
+  //   });
+  // });
+
+
+
+
 
--- ORM_Refactor/server/request-handler.js
+++ ORM_Refactor/server/request-handler.js
@@ -4,7 +4,33 @@ var helpers = require('./http-helpers');
 var database = require('./database');
 var url = require('url');
 
-var buildGetQueryString = function(req){
+var getMessages = function(req, response) {
+  console.log('GET attempt');
+  helpers.sendResponse(response, null);
+};
+
+var createMessage = function(req, response) {
+  console.log('post attempt');
+  helpers.sendResponse(response, null);
+};
+
+var corsOptions = function(req, response) {
+  console.log('OPTIONS attempt');
+  helpers.sendResponse(response, null);
+};
+
+var methods = {
+  'GET': getMessages,
+  'POST': createMessage,
+  'OPTIONS': corsOptions
+};
+
+module.exports = function(req, response) {
+  var method = methods[req.method];
+  method ? method(req, response) : helpers.sendResponse(response, null, 404);
+};
+
+/*
   var options = require('querystring').parse(req.url);
   var responseStr = "select b.username, a.text, c.roomname from messages a inner join users b on a.id_users = b.id inner join rooms c on c.id = a.id_rooms;";
 
@@ -28,214 +54,4 @@ var buildGetQueryString = function(req){
     responseStr = responseStr.substring(0, responseStr.length - 1); //slice off semi colon
     responseStr += " LIMIT 0, " + options.limit + ";";
   }
-
-  return responseStr;
-};
-var buildPostQueryString = function(message, cb){
-  var userId, roomId;
-
-  var lookupUserId = function(cb){
-    database.queryDB("select distinct id from users where username = \"" + message.username + "\";", function(err, rows, fields){
-      if(rows[0]) {
-        userId = rows[0].id;
-      }
-      cb();
-    });
-  };
-
-  var createNewUser = function(username, cb){
-    database.queryDB("insert into users (username, created_at) values ('" + username + "', now());", function(){
-        lookupUserId(cb);
-    });
-  };
-
-  var lookupRoomId = function(cb){
-    database.queryDB("select distinct id from rooms where roomname = \"" + message.roomname + "\";", function(err, rows, fields){
-      if(rows[0]) {
-        roomId = rows[0].id;
-      }
-      cb();
-    });
-  };
-
-  var createNewRoom = function(roomname, cb){
-    database.queryDB("insert into rooms (roomname) values ('" + roomname + "')", function(){
-        lookupRoomId(cb);
-    });
-  };
-
-  var continueQuery = function(){
-    lookupRoomId(function(){
-      if(!roomId) {
-        createNewRoom(message.roomname, endQuery);
-      } else {
-        endQuery();
-      }
-    });
-  };
-
-  var endQuery = function() {
-    database.queryDB('insert into messages (id_users, id_rooms, text, created_at) values (' + userId + ',' + roomId + ',\"' + message.text + '\", now());', function(){
-      console.log("user: " + userId + " and room: " + roomId);
-    });
-  };
-
-  lookupUserId(function(){
-    if(!userId) {
-      createNewUser(message.username, continueQuery);
-    } else {
-      continueQuery();
-    }
-  });
-};
-
-
-var getMessages = function(req, response) {
-  console.log('GET attempt');
-  var querystring = buildGetQueryString(req);
-  database.queryDB(querystring, function(err, data){
-    if(err) { throw err; }
-    helpers.sendResponse(response, {'results': data}, 200);
-  });
-};
-
-var createMessage = function(req, response) {
-  console.log('post attempt');
-  helpers.collectData(req, function(message){
-    buildPostQueryString(message, function(postQuery){
-      database.queryDB(postQuery, function(err, data){
-        if (err) throw err;
-        helpers.sendResponse(response, data, 200);
-      });
-    });
-
-  });
-  helpers.sendResponse(response, null);
-};
-
-var corsOptions = function(req, response) {
-  console.log('OPTIONS attempt');
-  helpers.sendResponse(response, null);
-};
-
-var methods = {
-  'GET': getMessages,
-  'POST': createMessage,
-  'OPTIONS': corsOptions
-};
-
-module.exports = function(req, response) {
-  var method = methods[req.method];
-  method ? method(req, response) : helpers.sendResponse(response, null, 404);
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var msgs = fs.readFileSync('storage.txt', 'utf8');
-
-// if(msgs[0] === ',') { //remove leading comma
-//   msgs = msgs.slice(1);
-// }
-// msgs = '[' + msgs + ']';
-
-// var storage = {"results": JSON.parse(msgs)};
-
-// var handleRequest = function(request, response) {
-
-//   var endResponse = function(statusCode, messages) {
-//     messages = messages || storage;
-//     var headers = defaultCorsHeaders;
-//     headers['Content-Type'] = "text/plain";
-//     response.writeHead(statusCode, headers);
-//     response.end(JSON.stringify(messages));
-//   };
-
-//   var buildCustomMessages = function(options) {
-//     var messages = {};
-
-//     //filter by roomname
-//     console.log(options);
-//     if('where' in options) {
-//       var roomname = JSON.parse(options.where).roomname;
-//       messages.results = _.filter(storage.results, function(message){
-//         if(message.roomname === roomname) {
-//           return message;
-//         }
-//       });
-//     } else {
-//       messages.results = storage.results.slice(0);
-//     }
-
-//     //order by
-//     if(options.order === '-createdAt') {
-//       messages.results.sort(function(a,b) {
-//         a = new Date(a.createdAt);
-//         b = new Date(b.createdAt);
-//         return b - a;
-//       });
-//     }
-
-//     // console.log(options.limit);
-//     if (options.limit){
-//       messages.results = messages.results.slice(0, options.limit);
-//     }
-
-//     return messages;
-//   }
-
-//   var url = require('url').parse(request.url);
-
-//   var urlParse = require('url').parse(request.url, true, true);
-
-//   if(url.path.slice(0, 8) !== '/classes'){
-//     endResponse(404);
-//   } else {
-//     if(request.method === 'OPTIONS') {
-//       endResponse(200);
-//     } else if (request.method === "POST" && url.path.slice(url.path.length - 5, url.path.length) === '/send'){
-//       var msg = "";
-//       request.on('data', function (data) {
-//         msg += data;
-//       });
-//       request.on('end', function() {
-//         msg = JSON.parse(msg);
-//         msg.createdAt = new Date();
-//         msg.updatedAt = new Date();
-//         storage.results.unshift(msg);
-//         fs.appendFile('storage.txt', ',' + JSON.stringify(msg),'utf8', function(){
-//           console.log("Storage updated with: ", JSON.stringify(msg));
-//         });
-//         endResponse(201);
-//       })
-//     } else if(request.method === 'GET'){
-//       var reqOptions = require('querystring').parse(url.path);
-//       var messages = buildCustomMessages(reqOptions);
-//       endResponse(200, messages);
-//     } else {
-//       endResponse(404);
-//     }
-//   }
-
-// };
-
-// var defaultCorsHeaders = {
-//   "access-control-allow-origin": "*",
-//   "access-control-allow-methods": "GET, POST, PUT, DELETE, OPTIONS",
-//   "access-control-allow-headers": "content-type, accept",
-//   "access-control-max-age": 10 // Seconds.
-// };
-
-// module.exports.handleRequest = handleRequest;
+*/
